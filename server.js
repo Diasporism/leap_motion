ef1733a9764cddb919d59bf4bacf15104ebc353b
Logan Sears <lsears@itriagehealth.com>
2014-12-21 11:56:48 -0700

    Configure application for Heroku. This involves modifying the package.json to include all forgotten dependencies and to have Heroku run Grunt to concatenate and uglify the JS files for production.

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ app.get('/', function(req, res){
   res.render('index.html');
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
